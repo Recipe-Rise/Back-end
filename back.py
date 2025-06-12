11c53280cac18ed73eedec48bfdf729b03704194
Potros Atia Ameen Soliman <153759544+Peter-Atia@users.noreply.github.com>
2025-06-12 19:32:15 +0300

    Update back.py

--- back.py
+++ back.py
@@ -131,4 +131,5 @@ def verify_email_method():
 
 # Run the application
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
